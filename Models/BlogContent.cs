93c239ad764447586483e28ca234da7d9ab3784f
Veysel GUNES <veysel.gunes36@gmail.com>
2020-07-08 09:09:50 +0300

    AdminBlog File Create

---
+++ Models/BlogContent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace AdminBlog.Models
+{
+    public class BlogContent:DbContext
+
+    { 
+        public BlogContent(DbContextOptions<BlogContent> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Author> Authors { set; get; }
+        public DbSet<Category> Categories { set; get; }
+        public DbSet<Blog> Blogs { set; get; }
+
+
+    }
+}
